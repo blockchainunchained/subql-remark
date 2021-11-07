9c7174ed71495f8a93d37ac3518fbb5b6871fa2f
BlockchainUnchained <bu@no>
2021-11-07 02:54:41 +0000

    Fix some issues with indexer. Ensure we check extrinsic is signed.

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -3,6 +3,11 @@ import {RemarkEntity} from "../types";
 
 export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
     const record = await RemarkEntity.get(extrinsic.block.block.header.hash.toString());
-    record.remark = extrinsic.extrinsic.args["_remark"].toString()
-    await record.save();
+    if(extrinsic.extrinsic.isSigned){
+        const index = extrinsic.extrinsic.args.findIndex(args => args.toString() === "_remark");
+        if (index >= 0) {
+            record.remark = extrinsic.extrinsic.args[index].toString()
+            await record.save();
+        }
+    }
 }
